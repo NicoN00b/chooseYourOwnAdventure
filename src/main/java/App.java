96cb15dc6150b587455bfa302ccab4163cc491fb
nr <nicholas.raethke@gmail.com>
2017-08-03 13:52:17 -0700

    add loops.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import Models.Game;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
@@ -8,16 +9,19 @@ import java.io.BufferedReader;
 public class App {
     public static void main(String[] args) {
         boolean GameStory = true;
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String input= "";
+
 
 
         try {
             while (GameStory){
                 System.out.println("Welcome to your Morning ennui.  Your alarm rudely woke you up and the Sun is blasting through your curtain, beckoning you to rouse.  What do you do?");
+                Game newGame = new Game();
                 System.out.println(" ");
-                System.out.println("Should you 'Get up' and face the day, or will you put your pillow over your head and go back to 'sleep' because there's nothing out there for you anyway.");
-                input = bufferedReader.readLine();
+                System.out.println("Should you 'get up' and face the day, or will you put your pillow over your head and go back to 'sleep' because there's nothing out there for you anyway.");
+                String playerResult = bufferedReader.readLine();
+                newGame.choiceEvaluator(playerOptions);
 
 
 
--- src/main/java/Models/Game.java
+++ src/main/java/Models/Game.java
@@ -2,12 +2,23 @@ package Models;
 
 public class Game {
 
-    private String [] options = {"get up", "sleep"};
-    private String input = null;
+    public String [] playerOptions = {"sleep", "get up"};
+    public String input = null;
+    public String playerResult = input;
 
-    public String choiceEvaluator(String choiceOne, String choiceTwo) {
-        choiceOne = options[0];
-        choiceTwo = options[1];
-        return choiceTwo;
+    public String choiceEvaluator() {
+//        choiceOne = options[0];
+//        choiceTwo = options[1];
+       for (Integer index = 0 ; index < playerOptions.length; index++) {
+           if (input.equals(playerOptions[0])) {
+               return "Good choice, in your sleep, you are free.";
+           } else if (input.equals(playerOptions[1])) {
+               return "The Slog Continues";
+
+           }
+
+       }
+       return "Game Over";
     }
 }
+
